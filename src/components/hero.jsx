dd331644e4fdf28fdad0b5f1d4201a852279e528
ian <ianalvesvalongo2008@gmail.com>
2024-06-19 14:23:29 -0300

    major media query update

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <>
       <div className="heroOa">
-        <h1>Empower Your Creativity</h1>
+        <h1>Empower Your <span>Creativity</span></h1>
         <p className="hero-subtitle">Unlock your potential with our innovative tools and resources, designed to inspire and elevate your creative projects.</p>
         <button className="hero-button">Join us now</button>
       </div>
